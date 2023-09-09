b21cb42ab943dbcdac3e0ce134aeb87501b16d39
Shuvo Khan <shuvoprogramer@gmail.com>
2023-09-09 23:57:07 +0600

    create folder structure

---
+++ src/app/routes/index.ts
@@ -0,0 +1,10 @@
+// import express from 'express';
+
+// const router = express.Router();
+
+// const moduleRoutes = [
+//   // ... routes
+// ];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.routes));
+// export default router;
